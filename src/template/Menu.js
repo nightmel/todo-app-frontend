5a470c9d2b9fdab2aa2c67a486acc26456f412d5
Caio Tieghi <ctieghi@rv3ti.com>
2019-03-26 14:27:37 -0300

    Título: Ajustes para o navbar. Index passou a ter o css do fontawesome. Menu precisou de classes a mais nas Tags: Nav, Li, A.

--- src/template/Menu.js
+++ src/template/Menu.js
@@ -8,14 +8,14 @@ function Menu (props) {
             <div className='container'>
                 <div className='navbar-header'>
                     <a className='navbar-brand' href='http://localhost:8080'>
-                        <i className='fas book-dead'></i>
+                        <i class="fab fa-accessible-icon"></i>
                     </a>
                 </div>
 
                 <div id='navbar' className='collapse navbar-collapse'>
                     <ul className='nav navbar-nav'>
-                        <li className='nav-item'><a  className='nav-link' href='#/todos'><i className='fas dragon'></i>Tarefas</a></li>
-                        <li className='nav-item'><a  className='nav-link' href='#/about'><i className='fas bullhorn'></i>Sobre</a></li>
+                        <li className='nav-item'><a  className='nav-link' href='#/todos'><i className='fas fa-dragon'></i> Tarefas</a></li>
+                        <li className='nav-item'><a  className='nav-link' href='#/about'><i className='fas fa-bullhorn'></i> Sobre</a></li>
                     </ul>
                 </div>
             </div>
